3e1e5589485f6da14fb8ff7b1ca6bef142c5d9d3
TheSpl4sh <thesplash007@gmail.com>
2024-06-01 18:21:11 +0300

    fixes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,51 +3,53 @@ import './Form.css'
 import { useTelegram } from '../../hooks/useTelegram'
 
 const Form = () => {
-  const [country, setCountry] = useState('')
-  const [street, setStreet] = useState('')
-  const [subject, setSubject] = useState('physical')
-  const {tg} = useTelegram()
+  const [country, setCountry] = useState('');
+  const [street, setStreet] = useState('');
+  const [subject, setSubject] = useState('physical');
+  const {tg} = useTelegram();
 
   const onSendData = useCallback(() => {
-    const data = {
-      country,
-      street,
-      subject
-    }
-    tg.sendData(JSON.stringify(data))
+      const data = {
+          country,
+          street,
+          subject
+      }
+      tg.sendData(JSON.stringify(data));
+  }, [country, street, subject])
+
+  useEffect(() => {
+      tg.onEvent('mainButtonClicked', onSendData)
+      return () => {
+          tg.offEvent('mainButtonClicked', onSendData)
+      }
+  }, [onSendData])
+
+  useEffect(() => {
+      tg.MainButton.setParams({
+          text: 'Отправить данные'
+      })
   }, [])
 
   useEffect(() => {
-    tg.onEvent('MainButtonClicked', onSendData)
-    return () => {
-      tg.offEvent('MainButtonClicked', onSendData)
-    }
-  }, [])
-
-  useEffect(() => {
-    tg.MainButton.setParams({
-      text: 'Send data'
-    })
-  }, [])
-
-  useEffect(() => {
-    if(!street || !country ) {
-      tg.MainButton.hide()
-    } else {
-      tg.MainButton.show()
-    }
+      if(!street || !country) {
+          tg.MainButton.hide();
+      } else {
+          tg.MainButton.show();
+      }
   }, [country, street])
 
   const onChangeCountry = (e) => {
-    setCountry(e.target.value) 
+      setCountry(e.target.value)
   }
 
   const onChangeStreet = (e) => {
-    setStreet(e.target.value) 
+      setStreet(e.target.value)
   }
+
   const onChangeSubject = (e) => {
-    setSubject(e.target.value) 
+      setSubject(e.target.value)
   }
+
 
   return (
     <div className={'form'}>
