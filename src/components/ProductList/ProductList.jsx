2f17c20f4674e9ef4432e4fbfc19571e075dfceb
TheSpl4sh <thesplash007@gmail.com>
2024-06-02 20:13:02 +0300

    fix

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,26 +3,24 @@ import './ProductList.css'
 import ProductItem from '../ProductItem/ProductItem'
 import {useTelegram} from '../../hooks/useTelegram'
 
+const products = [
+  {id: '1', title: 'Jeans', price: 5000, description: 'Blue, straight'},
+  {id: '2', title: 'Jacket', price: 12000, description: 'Green, warm'},
+  {id: '3', title: 'Jeans 2', price: 5000, description: 'Blue, straight'},
+  {id: '4', title: 'Jacket 8', price: 122, description: 'Green, warm'},
+  {id: '5', title: 'Jeans 3', price: 5000, description: 'Blue, straight'},
+  {id: '6', title: 'Jacket 7', price: 600, description: 'Green, warm'},
+  {id: '7', title: 'Jeans 4', price: 5500, description: 'Blue, straight'},
+  {id: '8', title: 'Jacket 5', price: 12000, description: 'Green, warm'},
+]
 
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, item) => {
+    return acc += item.price
+  })
+}
 
 const ProductList = () => {
-  const products = [
-    {id: '1', title: 'Jeans', price: 5000, description: 'Blue, straight'},
-    {id: '2', title: 'Jacket', price: 12000, description: 'Green, warm'},
-    {id: '3', title: 'Jeans 2', price: 5000, description: 'Blue, straight'},
-    {id: '4', title: 'Jacket 8', price: 122, description: 'Green, warm'},
-    {id: '5', title: 'Jeans 3', price: 5000, description: 'Blue, straight'},
-    {id: '6', title: 'Jacket 7', price: 600, description: 'Green, warm'},
-    {id: '7', title: 'Jeans 4', price: 5500, description: 'Blue, straight'},
-    {id: '8', title: 'Jacket 5', price: 12000, description: 'Green, warm'},
-  ]
-  
-  const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
-      return acc += item.price
-    })
-  }
-
   const [addedItems, setAddedItems] = useState([])
   const{tg} = useTelegram()
 
