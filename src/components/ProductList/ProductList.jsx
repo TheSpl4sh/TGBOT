ed27642815292287e1d8e38cdc8dece139445f28
TheSpl4sh <thesplash007@gmail.com>
2024-06-02 19:22:40 +0300

    feat: productList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './ProductList.css'
 import ProductItem from '../ProductItem/ProductItem'
+import {useTelegram} from '../../hooks/useTelegram'
 
 const products = [
   {id: '1', title: 'Jeans', price: 5000, description: 'Blue, straight'},
@@ -13,10 +14,37 @@ const products = [
   {id: '8', title: 'Jacket 5', price: 12000, description: 'Green, warm'},
 ]
 
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, item) => {
+    return acc += item.price
+  })
+}
+
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([])
+  const{tg} = useTelegram()
 
-  
+  const onAdd = (product) => {
+    const alreadyAdded = addedItems.find(item => item.id === product.id);
+      let newItems = [];
+
+      if(alreadyAdded) {
+        newItems = addedItems.filter(item => item.id !== product.id);
+      } else {
+        newItems = [...addedItems, product];
+      }
+
+      setAddedItems(newItems)
+
+      if(newItems.length == 0) {
+        tg.MainButton.hide()
+      } else {
+        tg.MainButton.show()
+        RegExp.MainButton.setParams({
+          text: `Buy ${getTotalPrice(newItems)}`
+        })
+      }
+  }
 
   return (
     <div className={'list'}>
