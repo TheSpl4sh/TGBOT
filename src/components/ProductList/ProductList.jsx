9dadd98fe27ba2c17a28df16fc94a00799186314
TheSpl4sh <thesplash007@gmail.com>
2024-06-03 23:12:00 +0300

    some fixes

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
         totalPrice: getTotalPrice(data),
         queryId 
     }
-    fetch('http://localhost:8000', {
+    fetch('http://localhost:8000/web-data', {
       method: POST,
       headers: {
         'Content-Type': 'application/json',
