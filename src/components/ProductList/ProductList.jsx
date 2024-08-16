e5aa927c995e9fcace435e81f880448a3922375a
TheSpl4sh <thesplash007@gmail.com>
2024-08-16 19:51:26 +0300

    feat: adding ip in ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
         totalPrice: getTotalPrice(data),
         queryId 
     }
-    fetch('http://localhost:8000/web-data', {
+    fetch('http://176.118.167.142:8000/web-data', {
       method: POST,
       headers: {
         'Content-Type': 'application/json',
